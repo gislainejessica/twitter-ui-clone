cbdafb3d0686ad30d31b3d7fd0df00d6ae16c29c
GislaineJessica <GislainehJessica@gmail.com>
2020-07-11 21:09:27 -0300

    profile

--- twitter/src/components/ProfilePage/index.tsx
+++ twitter/src/components/ProfilePage/index.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Banner, Avatar, ProfileData, LocationIcon, CakeIcon, FollowAge } from './styles';
+//import { Edit } from 'styled-icons/material-outlined';
 
 const ProfilePage: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        {/* <EditButton outlined> Editar Perifil</EditButton> */}
+        <h1>Gislaine Jèssica</h1>
+        <h2>@jessy_code</h2>
+
+        <p>
+          Developer at <a href="https://udna.life"> @udna </a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Vitória, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 18 de Abril de 1989
+          </li>
+        </ul>
+        <FollowAge>
+          <span> seguindo <strong> 87</strong></span>
+          <span> <strong>789</strong> seguidores </span>
+        </FollowAge>
+      </ProfileData>
+    </Container>
+  );
 }
 
 export default ProfilePage;
